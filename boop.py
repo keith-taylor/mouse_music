ebe2a111efa67e374eb8d0c3769faaa433450ef4
Keith Taylor <k-taylor@outlook.com>
2023-02-23 12:48:26 +0000

    Updating comments

--- boop.py
+++ boop.py
@@ -6,5 +6,13 @@ my_list_1 = [1, 2, 3, 4, 5, 6]
 my_list_2 = []
 
 # comment 1
+# This is the contents of comment one
+
 # comment 2
+# This is the contents of comment 2.
+
 # comment 3
+# This is ccomments of three. 
+
+# comment 4
+# this i s new comments
